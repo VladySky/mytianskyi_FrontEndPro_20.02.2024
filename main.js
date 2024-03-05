257f8141b5f2baebb978805217c9e01229eb607f
Vlady <vlad.mytia@gmail.com>
2024-03-05 21:13:27 +0100

    final

--- main.js
+++ main.js
@@ -1,51 +1,50 @@
-let year = +prompt('Скільки тобі років ?');
-if (year === 0) {
-    alert('Шкода, що Ви не захотіли ввести свій вік');
-} else {
-    alert(`Тобто, тобі ${year} років`);
-}
+ let year = +prompt('Скільки тобі років ?');
+ if (year === 0) {
+     alert('Шкода, що Ви не захотіли ввести свій вік');
+ } else {
+     alert(`Тобто, тобі ${year} років`);
+ }
 
-const city = prompt( 'В якому місті ти народився ?');
+const city = prompt('В якому місті ти народився ?');
 
-
-let capital = city;
-
-if(capital === null){
+if (city === null || city.trim() === '') {
     alert('Шкода, що Ви не захотіли ввести своє місто');
-}
-
-switch (capital) {
-    case capital = 'Київ' :
-        capital = alert(`Ти живеш в столиці Київ`);
-        break;
-    case capital = 'Лондон':
-        capital = alert(`Ти живеш в столиці Лондон`);
-        break;
-    case capital = 'Вашингтон':
-        capital = alert(`Ти живеш в столиці Вашингтон`);
-        break;
-    default :
-        alert(`Ти живеш в місті ${city}`);
-        break;
+} else {
+    switch (city) {
+        case 'Київ':
+            alert(`Ти живеш в столиці Київ`);
+            break;
+        case 'Лондон':
+            alert(`Ти живеш в столиці Лондон`);
+            break;
+        case 'Вашингтон':
+            alert(`Ти живеш в столиці Вашингтон`);
+            break;
+        default:
+            alert(`Ти живеш в місті ${city}`);
+    }
 }
 
 
 
 let sport = prompt('Який твій улюблений вид спорту ?');
-let kindSport = sport;
-switch (kindSport) {
-    case kindSport = 'Футбол' :
-        alert('Круто! Хочеш стати як Роналдо?');
-        break;
-    case kindSport = 'Баскетбол' :
-        alert('Круто! Хочеш стати як ЛеБрон Джеймс?');
-        break;
-    case kindSport = 'Стрільба з луку' :
-        alert('Круто! Хочеш стати Робін Гудом?');
-        break
-    default :
-        alert('Я не знаю відомих спортсменів в цьому виді спорту')
-
+if (sport === null || sport.trim() === ''){
+    alert('Шкода, що Ви не захотіли ввести свій улюблений вид спорту');
+} else {
+    switch (sport) {
+        case sport = 'Футбол' :
+            alert('Круто! Хочеш стати як Роналдо?');
+            break;
+        case sport = 'Баскетбол' :
+            alert('Круто! Хочеш стати як ЛеБрон Джеймс?');
+            break;
+        case sport = 'Стрільба з луку' :
+            alert('Круто! Хочеш стати Робін Гудом?');
+            break
+        default :
+            alert('Я не знаю відомих спортсменів в цьому виді спорту');
+            break;
+    }
 }
 
 
